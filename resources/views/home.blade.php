ff461b3dcab08cb2406d6fa6106ef06cf1b81093
Henriquegab <henriquepro8@gmail.com>
2022-03-08 02:11:26 -0300

    oi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,49 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    
+    @php
+        
+
+
+
+        $list = [6, 4, 7, 9, 3, 12];
+
+$results = [];
+
+for($i = 0; $i < count($list); $i++){
+
+	for($j = 0; $j < count($list) ; $j++){
+
+		if(!($i == $j)){
+			
+			if(($list[$i] + $list[$j]) == 13 ){
+				
+
+			array_push($results, "$list[$i] + $list[$j]");
+
+			}
+		
+
+		}
+
+
+	}
+
+
+}
+
+print_r($results);
+
+
+
+
+
+
+
+    @endphp
+
+
 @stop
 
 @section('css')
